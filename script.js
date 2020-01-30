9c73b667c77a508052c3b1df7be98bb07d22758d
Nolan <noltron000@gmail.com>
2020-01-29 22:47:26 -0800

    [add] connecting the dots
    linked javascript, html, and css...but having trouble with async in scripts.js

---
+++ script.js
@@ -0,0 +1,30 @@
+class Character {
+	constructor() {
+		Promise.all([
+			fetch('./races.json'),
+			fetch('./subraces.json'),
+			fetch('./backgrounds.json'),
+		])
+		.then(responses => responses.map(res => res.json()))
+		.then((data) => {
+			[ // set class attributes via deconstruction
+				this.raceJSON,
+				this.subraceJSON,
+				this.backgroundJSON,
+			] = data
+			// this is where the "true" constructor begins
+			this._rollCharacter()
+		})
+		.catch((error) => {
+			console.error("Oh no! Something terrible happened.")
+			new Error(error.message)
+		})
+	}
+
+	_rollCharacter = () => {
+		console.warn(this.raceJSON)
+		console.warn(this.raceJSON["human"])
+	}
+}
+
+const Vlanljorg = new Character

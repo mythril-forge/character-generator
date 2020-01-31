1805580128d1ab1cb3eb85c328e326d48c0070f0
Nolan <noltron000@gmail.com>
2020-01-30 17:58:52 -0800

    [add] this.info for character information

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 class Character {
 	constructor() {
-		this.abilities = {
-			'strength': 0,
-			'dexterity': 0,
-			'constitution': 0,
-			'intelligence': 0,
-			'wisdom': 0,
-			'charisma': 0,
+		this.info = {
+			'race': null,
+			'subrace': null,
+			'background': null,
+			'abilities': {
+				'strength': 0,
+				'dexterity': 0,
+				'constitution': 0,
+				'intelligence': 0,
+				'wisdom': 0,
+				'charisma': 0,
+			},
+			'proficiencies': {
+				'skills': new Set(),
+				'tools': new Set(),
+				'languages': new Set(),
+			},
 		}
 
 		Promise.all([
@@ -22,6 +32,7 @@ class Character {
 			] = data
 			// this is where the "true" constructor begins
 			this._rollCharacter()
+			this._parseFeatures()
 		})
 		.catch((error) => {
 			console.error("Oh no! Something terrible happened.")
@@ -29,22 +40,33 @@ class Character {
 		})
 	}
 
+
 	_rollCharacter = () => {
 		// choose race
-		this.race = [...rollKeys(1, this.raceData)][0]
-		// choose subrace if applicable
-		if (this.race in this.subraceData) {
-			const subraces = this.subraceData[this.race]
-			this.subrace = [...rollKeys(1, subraces)][0]
-		} else {
-			this.subrace = null
-		}
-		// choose background
-		this.background = [...rollKeys(1, this.backgroundData)][0]
+		this.info['race'] = [...rollKeys(1, this.raceData)][0]
 
-		console.log(this.subrace, this.race, this.background)
+		// choose subrace if applicable
+		if (this.info['race'] in this.subraceData) {
+			const subraces = this.subraceData[this.info['race']]
+			this.info['subrace'] = [...rollKeys(1, subraces)][0]
+		} else {
+			this.info['subrace'] = null
+		}
+
+		// choose background
+		this.info['background'] = [...rollKeys(1, this.backgroundData)][0]
+	}
+
+
+	_parseFeatures = () => {
+		// ability scores
+		// ability score bonuses
+		// proficiencies
+		// extra proficiencies
+		// text-based features
 	}
 }
+
 
 const rollDice = (count, size, random = Math.random) => {
 	const results = []
@@ -54,6 +76,7 @@ const rollDice = (count, size, random = Math.random) => {
 	}
 	return results
 }
+
 
 const rollKeys = (count, object, random = Math.random) => {
 	const keys = Object.keys(object)
@@ -65,5 +88,6 @@ const rollKeys = (count, object, random = Math.random) => {
 	}
 	return results
 }
+
 
 const Vlanljorg = new Character

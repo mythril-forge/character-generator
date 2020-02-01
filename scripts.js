36fd2372f12747e08ace549db0504111efa2d7b9
Nolan <noltron000@gmail.com>
2020-02-01 14:51:13 -0800

    [add] populate more of index.html

--- scripts.js
+++ scripts.js
@@ -1,8 +1,52 @@
 const UserChar = new Character()
 
 const displayInfo = () => {
+	// reroll character
 	UserChar.generate()
-	const raceEl = document.getElementById('race')
-	raceEl.textContent = UserChar.info['race']
-	console.log(UserChar.info['race'])
+
+	/* RACE & SUBRACE */
+	// generate race text
+	let raceText = ''
+	if (UserChar.info['subrace'] !== null) {
+		raceText += UserChar.info['subrace']
+		raceText += ' '
+	}
+	raceText += UserChar.info['race']
+	// apply to elements
+	const raceEls = document.getElementsByClassName('race-info')
+	for (const el of raceEls) {
+		el.textContent = raceText
+	}
+
+	/* BACKGROUND */
+	// generate background text
+	let backText = UserChar.info['background']
+	// apply to elements
+	const backEls = document.getElementsByClassName('background-info')
+	for (const el of backEls) {
+		el.textContent = backText
+	}
+
+	/* ABILITY SCORES */
+	// identify key info
+	let abilityInfo = UserChar.info['ability scores']
+	abilityInfo = Object.entries(abilityInfo)
+	for (const [ability, score] of abilityInfo) {
+		// generate ability score text
+		let bonusText = Math.floor((score - 10) / 2)
+		if (bonusText > 0) {
+			bonusText = '+' + bonusText.toString()
+		} else if (bonusText < 0) {
+			bonusText = '−' + Math.abs(bonusText).toString()
+		} else {
+			bonusText = '±' + bonusText.toString()
+		}
+		const abilityText = score.toString() + ' (' + bonusText + ')'
+		// apply to elements
+		const classKey = ability + '-score-info'
+		const abilityEls = document.getElementsByClassName(classKey)
+		for (const el of abilityEls) {
+			el.textContent = abilityText
+		}
+	}
 }
